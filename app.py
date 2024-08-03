3b6946be2c5abd9953b2f4472c22d40dc60d03c2
Abdul Manan <147904484+AbdulManan001@users.noreply.github.com>
2024-08-03 23:27:51 +0500

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def transform_text(text):
 tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
 model = pickle.load(open('model.pkl', 'rb'))
 
-st.title('Spam Classifier')
+st.title('SMS Spam Classifier')
 input_sms = st.text_area("Enter the message to classify")
 
 if st.button('Predict'):
